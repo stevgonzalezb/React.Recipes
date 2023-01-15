5993e5e2688c112bbfa6f22d6f2b7bafa991dd29
Steven <stevengonzalez957@gmail.com>
2023-01-14 18:47:15 -0600

    logo stuff

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
+import Logo from '../../assets/logo-no-background.png'
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
@@ -28,12 +30,12 @@ export default function Example() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    src={Logo}
                     alt="Your Company"
                   />
                   <img
                     className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    src={Logo}
                     alt="Your Company"
                   />
                 </div>
