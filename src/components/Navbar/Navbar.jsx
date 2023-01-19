4252145cca93a6310aa4caf53d9fda512494d711
Steven Gonzalez <stevengonzalez957@gmail.com>
2023-01-18 21:09:01 -0600

    navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 
 import Logo from '../../assets/logo-no-background.png'
 import { Link } from "react-router-dom";
@@ -6,46 +6,23 @@ import { Link } from "react-router-dom";
 
 export default function Example() {
   return (
-    <nav class="bg-white shadow">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+  <nav class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 justify-between">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
         {/* <!-- Mobile menu button --> */}
-        <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          {/* <!--
-            Icon when menu is closed.
-
-            Heroicon name: outline/bars-3
-
-            Menu open: "hidden", Menu closed: "block"
-          --> */}
-          <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          {/* <!--
-            Icon when menu is open.
-
-            Heroicon name: outline/x-mark
-
-            Menu open: "block", Menu closed: "hidden"
-          --> */}
-          <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+        <button type="button" class="inline-flex items-center justify-center rounded-md p-2 hover:bg-yellow-200 text-gray-700 " aria-controls="mobile-menu" aria-expanded="false">
+            <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
         </button>
       </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start gap-6">
         <div class="flex flex-shrink-0 items-center">
           <img class="block h-8 w-auto lg:hidden" src={Logo} alt="Your Company"/>
           <img class="hidden h-8 w-auto lg:block" src={Logo} alt="Your Company"/>
         </div>
-        <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
-          {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
-          <a href="#" class="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900">Dashboard</a>
-          <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Team</a>
-          <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Projects</a>
-          <a href="#" class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Calendar</a>
+        <div class="hidden sm:flex sm:gap-4">
+          <Link to="/" className="inline-flex items-center border-b-2 border-yellow-600 p-4 text-sm font-bold text-gray-700">Recipes</Link>
+          <Link to="/" className="inline-flex items-center border-b-2 border-transparent p-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Drinks</Link>
         </div>
       </div>
     </div>
@@ -53,12 +30,12 @@ export default function Example() {
 
   {/* <!-- Mobile menu, show/hide based on menu state. --> */}
   <div class="hidden" id="mobile-menu">
-    <div class="flex flex-col gap-2 text-gray-700 font-medium">
-      <Link to="/" className="border-l-4 border-yellow-600 bg-yellow-50 py-4 px-2">Recipes</Link>
-      {/* <Link to="/" className="border-l-4 border-transparent py-4 px-2 hover:border-gray-300 hover:bg-yellow-50">Drinks</Link> */}
+    <div class="flex flex-col gap-2 font-medium">
+      <Link to="/" className="border-l-4 text-gray-700 border-yellow-600 bg-yellow-100 py-4 px-2 font-bold">Recipes</Link>
+      <Link to="/" className="border-l-4 text-gray-500 border-transparent py-4 px-2 hover:text-gray-700 hover:border-gray-300 hover:bg-yellow-100">Drinks</Link>
     </div>
   </div>
-</nav>
+  </nav>
 
   )
 }
