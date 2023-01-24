58a2db5fc54b9921ddff59f17346a5431bb244e5
Steven Gonzalez <stevengonzalez957@gmail.com>
2023-01-24 08:31:50 -0600

    filters sidebar progress

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,73 +1,69 @@
-// import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon } from '@heroicons/react/24/outline'
+import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon, FunnelIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
 
-// const navigation = [
-//   { name: 'Dashboard', icon: HomeIcon, href: '#', current: true },
-//   { name: 'Team', icon: UsersIcon, href: '#', count: 3, current: false },
-//   { name: 'Projects', icon: FolderIcon, href: '#', count: 4, current: false },
-//   { name: 'Calendar', icon: CalendarIcon, href: '#', current: false },
-//   { name: 'Documents', icon: InboxIcon, href: '#', count: 12, current: false },
-//   { name: 'Reports', icon: ChartBarIcon, href: '#', current: false },
-// ]
+const navigation = [
+  { name: 'Dashboard', icon: HomeIcon, href: '#', current: true },
+  { name: 'Team', icon: UsersIcon, href: '#', count: 3, current: false },
+  { name: 'Projects', icon: FolderIcon, href: '#', count: 4, current: false },
+  { name: 'Calendar', icon: CalendarIcon, href: '#', current: false },
+  { name: 'Documents', icon: InboxIcon, href: '#', count: 12, current: false },
+  { name: 'Reports', icon: ChartBarIcon, href: '#', current: false },
+]
 
-// function classNames(...classes) {
-//   return classes.filter(Boolean).join(' ')
-// }
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
-// export default function Example() {
-//   return (
-//     <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
-//       <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
-//         <nav className="mt-5 flex-1 space-y-1 bg-white px-2" aria-label="Sidebar">
-//           {navigation.map((item) => (
-//             <a
-//               key={item.name}
-//               href={item.href}
-//               className={classNames(
-//                 item.current
-//                   ? 'bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100'
-//                   : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50',
-//                 'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-//               )}
-//             >
-//               <item.icon
-//                 className={classNames(
-//                   item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-//                   'mr-3 flex-shrink-0 h-6 w-6'
-//                 )}
-//                 aria-hidden="true"
-//               />
-//               <span className="flex-1">{item.name}</span>
-//               {item.count ? (
-//                 <span
-//                   className={classNames(
-//                     item.current ? 'bg-white' : 'bg-gray-100 group-hover:bg-gray-200',
-//                     'ml-3 inline-block py-0.5 px-3 text-xs font-medium rounded-full'
-//                   )}
-//                 >
-//                   {item.count}
-//                 </span>
-//               ) : null}
-//             </a>
-//           ))}
-//         </nav>
-//       </div>
-//       <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-//         <a href="#" className="group block w-full flex-shrink-0">
-//           <div className="flex items-center">
-//             <div>
-//               <img
-//                 className="inline-block h-9 w-9 rounded-full"
-//                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-//                 alt=""
-//               />
-//             </div>
-//             <div className="ml-3">
-//               <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-//               <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-//             </div>
-//           </div>
-//         </a> 
-//       </div>
-//     </div>
-//   )
-// }
+export default function Sidebar() {
+    
+    // Mobile Sidebar flag.
+    const [sidebar, setSidebar] = useState(false)
+
+    return (
+        <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
+
+            {/* Sidebar */}
+            <div className={ sidebar ? "sm:hidden" : "hidden" }>
+                <nav className="flex-1 bg-white" aria-label="Sidebar">
+                {navigation.map((item) => (
+                    <a
+                    key={item.name}
+                    href={item.href}
+                    className={classNames(
+                        item.current
+                        ? 'bg-gray-100 text-gray-900 hover:text-gray-900 hover:bg-gray-100'
+                        : 'text-gray-600 hover:text-gray-900 hover:bg-gray-50',
+                        'group flex items-center text-sm font-medium rounded-md'
+                    )}
+                    >
+                    <item.icon
+                        className={classNames(
+                        item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
+                        'flex-shrink-0 h-6 w-6'
+                        )}
+                        aria-hidden="true"
+                    />
+                    <span className="flex-1">{item.name}</span>
+                    {item.count ? (
+                        <span
+                        className={classNames(
+                            item.current ? 'bg-white' : 'bg-gray-100 group-hover:bg-gray-200',
+                            'inline-block py-0.5 px-3 text-xs font-medium rounded-full'
+                        )}
+                        >
+                        {item.count}
+                        </span>
+                    ) : null}
+                    </a>
+                ))}
+                </nav>
+            </div>
+
+            {/* Mobile toggle button */}
+            <div className='h-10 w-10 rounded-r-xl flex justify-center bg-gray-500 lg:hidden'
+                onClick={ () => setSidebar(!sidebar) }>
+                <FunnelIcon className='w-6 text-white' />
+            </div>
+        </div>
+    )
+}
