63ebf37845d81bb27bed61773a9e15f77dc9dc82
Steven Gonzalez <stevengonzalez957@gmail.com>
2023-01-29 19:37:38 -0600

    icons

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -1,7 +1,5 @@
-import { XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3BottomLeftIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
-
-import Logo from '../../assets/logo-no-background.png'
 
 export default function Sidebar() {
     
@@ -14,10 +12,7 @@ export default function Sidebar() {
                 <div class="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
                 <button type="button" class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden">
                     <span class="sr-only">Open sidebar</span>
-                    {/* <!-- Heroicon name: outline/bars-3-bottom-left --> */}
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-                    </svg>
+                    <Bars3BottomLeftIcon className="block h-6 w-6"/>
                 </button>
                 <div class="flex flex-1 justify-between px-4">
                     <div class="flex flex-1">
@@ -26,9 +21,7 @@ export default function Sidebar() {
                         <div class="relative w-full text-gray-400 focus-within:text-gray-600">
                         <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center">
                             {/* <!-- Heroicon name: mini/magnifying-glass --> */}
-                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-                            </svg>
+                            <MagnifyingGlassIcon className="block h-6 w-6"/>
                         </div>
                         <input id="search-field" class="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm" placeholder="Search" type="search" name="search"/>
                         </div>
