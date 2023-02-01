e2077fa5906d209f4205ebe068a0ff1aea1a6472
Steven Gonzalez <stevengonzalez957@gmail.com>
2023-02-01 15:35:04 -0600

    styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,24 +16,24 @@ export default function Sidebar({ closeSidebar, isOpen, handleSelectFilter}) {
 
     return (
         <> 
-            <div className='flex flex-row bg-gray-50'>
-                <div className="h-screen p-2 border-r border-gray-200 md:flex w-72 hidden">
-                    <div className="flex flex-grow flex-col gap-4 overflow-y-auto bg-gray-50">
+            <div className='flex flex-row bg-gray-200'>
+                <div className="h-screen p-2 border-r border-gray-400 md:flex w-72 hidden">
+                    <div className="flex flex-grow flex-col gap-4 overflow-y-auto bg-gray-200">
                         <div className="flex flex-shrink-0 justify-center items-center ">
                             <img className="h-12 w-auto" src={Logo} alt="Cookify "/>
                         </div>
-                        <hr/>
+                        <div className="h-[1px] bg-gray-400"></div>
                         <div className="flex flex-grow flex-col gap-2">
 
                             <div className='flex flex-col'>
-                                <div className={ (open === 'allergies' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-200 cursor-pointer'} 
+                                <div className={ (open === 'allergies' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-700 cursor-pointer'} 
                                         onClick={() =>  open === 'allergies' ? setOpen('') : setOpen('allergies')}>
-                                    <div className='font-bold text-lg text-gray-600'>Allergies</div>
+                                    <div className='font-bold text-normal text-gray-50'>Allergies</div>
                                     <div className='flex flex-row items-center justify-center gap-1'>
-                                        <div className='bg-gray-700 rounded-full h-5 w-5 flex items-center justify-center'>
-                                            <div className=' text-white text-sm'>2</div>
+                                        <div className='bg-yellow-600 rounded-full h-5 w-5 flex items-center justify-center'>
+                                            <div className=' text-white text-sm font-semibold'>2</div>
                                         </div>
-                                        <ChevronDownIcon className='h-4 w-4' />
+                                        <ChevronDownIcon className='h-4 w-4 text-white' />
                                     </div>
                                 </div>
                                 <div className={(open === 'allergies' ? 'flex': 'hidden') + ' rounded-b-lg flex-row gap-2 p-2 flex-wrap bg-gray-100'}>
@@ -46,14 +46,14 @@ export default function Sidebar({ closeSidebar, isOpen, handleSelectFilter}) {
                             </div>
 
                             <div>
-                            <div className={ (open === 'diets' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-200 cursor-pointer'} 
-                                        onClick={() => open === 'diets' ? setOpen('') : setOpen('diets')}>
-                                    <div className='font-bold text-lg text-gray-600'>Diets</div>
+                                <div className={ (open === 'diets' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-700 cursor-pointer'} 
+                                        onClick={() =>  open === 'diets' ? setOpen('') : setOpen('diets')}>
+                                    <div className='font-bold text-normal text-gray-50'>Diets</div>
                                     <div className='flex flex-row items-center justify-center gap-1'>
-                                        <div className='bg-gray-700 rounded-full h-5 w-5 flex items-center justify-center'>
-                                            <div className=' text-white text-sm'>2</div>
+                                        <div className='bg-yellow-600 rounded-full h-5 w-5 flex items-center justify-center'>
+                                            <div className=' text-white text-sm font-semibold'>2</div>
                                         </div>
-                                        <ChevronDownIcon className='h-4 w-4' />
+                                        <ChevronDownIcon className='h-4 w-4 text-white' />
                                     </div>
                                 </div>
                                 <div className={(open === 'diets' ? 'flex': 'hidden') + ' rounded-b-lg flex-row gap-2 p-2 flex-wrap bg-gray-100'}>
@@ -69,7 +69,7 @@ export default function Sidebar({ closeSidebar, isOpen, handleSelectFilter}) {
                 </div>
 
                 <div className={ isOpen ? "md:hidden flex absolute top-0 h-screen w-full z-40 right-0 gap-8 justify-start flex-1 bg-gray-600 bg-opacity-75" : "hidden absolute top-0 h-full w-full z-40 right-0 gap-8 justify-start flex-1 bg-gray-600 bg-opacity-75" }>
-                    <div className="h-screen  border-r border-gray-200 flex w-72">
+                    <div className="h-screen  border-r border-gray-200 flex w-4/5 ">
                         <div className="flex p-2 flex-grow flex-col gap-2 overflow-y-auto bg-white">
                             <div className="flex flex-shrink-0 justify-center items-center">
                                 <img className="h-12 w-auto" src={Logo} alt="Cookify"/>
@@ -79,16 +79,16 @@ export default function Sidebar({ closeSidebar, isOpen, handleSelectFilter}) {
                             <div className="flex flex-grow flex-col gap-2">
 
                                 <div className='flex flex-col'>
-                                    <div className={ (open === 'allergies' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-200 cursor-pointer'} 
-                                            onClick={() =>  open === 'allergies' ? setOpen('') : setOpen('allergies')}>
-                                        <div className='font-bold text-lg text-gray-600'>Allergies</div>
-                                        <div className='flex flex-row items-center justify-center gap-1'>
-                                            <div className='bg-gray-700 rounded-full h-5 w-5 flex items-center justify-center'>
-                                                <div className=' text-white text-sm'>2</div>
-                                            </div>
-                                            <ChevronDownIcon className='h-4 w-4' />
+                                <div className={ (open === 'allergies' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-700 cursor-pointer'} 
+                                        onClick={() =>  open === 'allergies' ? setOpen('') : setOpen('allergies')}>
+                                    <div className='font-bold text-normal text-gray-50'>Allergies</div>
+                                    <div className='flex flex-row items-center justify-center gap-1'>
+                                        <div className='bg-yellow-600 rounded-full h-5 w-5 flex items-center justify-center'>
+                                            <div className=' text-white text-sm font-semibold'>2</div>
                                         </div>
+                                        <ChevronDownIcon className='h-4 w-4 text-white' />
                                     </div>
+                                </div>
                                     <div className={(open === 'allergies' ? 'flex': 'hidden') + ' rounded-b-lg flex-row gap-2 p-2 flex-wrap bg-gray-100'}>
                                         {
                                             FILTERS.Allergies.map((value) => (
@@ -99,16 +99,16 @@ export default function Sidebar({ closeSidebar, isOpen, handleSelectFilter}) {
                                 </div>
 
                                 <div>
-                                <div className={ (open === 'diets' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-200 cursor-pointer'} 
-                                            onClick={() => open === 'diets' ? setOpen('') : setOpen('diets')}>
-                                        <div className='font-bold text-lg text-gray-600'>Diets</div>
-                                        <div className='flex flex-row items-center justify-center gap-1'>
-                                            <div className='bg-gray-700 rounded-full h-5 w-5 flex items-center justify-center'>
-                                                <div className=' text-white text-sm'>2</div>
-                                            </div>
-                                            <ChevronDownIcon className='h-4 w-4' />
+                                <div className={ (open === 'diets' ? 'rounded-t-lg': 'rounded-lg') + ' flex flex-row items-center justify-between p-2 bg-gray-700 cursor-pointer'} 
+                                        onClick={() =>  open === 'diets' ? setOpen('') : setOpen('diets')}>
+                                    <div className='font-bold text-normal text-gray-50'>Diets</div>
+                                    <div className='flex flex-row items-center justify-center gap-1'>
+                                        <div className='bg-yellow-600 rounded-full h-5 w-5 flex items-center justify-center'>
+                                            <div className=' text-white text-sm font-semibold'>2</div>
                                         </div>
+                                        <ChevronDownIcon className='h-4 w-4 text-white' />
                                     </div>
+                                </div>
                                     <div className={(open === 'diets' ? 'flex': 'hidden') + ' rounded-b-lg flex-row gap-2 p-2 flex-wrap bg-gray-100'}>
                                         {
                                             FILTERS.Diets.map((value) => (
