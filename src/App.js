aaaa035d6bdc3ed5967bed5e239010853dcceea2
Steven Gonzalez <stevengonzalez957@gmail.com>
2023-02-24 16:31:14 -0600

    remove sidebar for the first version

--- src/App.js
+++ src/App.js
@@ -8,14 +8,12 @@ function App() {
 
   // Mobile Sidebar flag.
   const [sidebar, setSidebar] = useState(false)
-  // let [filters, setFilters] = useState(['Crustacean-free'])
-  let [filters, setFilters] = useState(0)
 
   return (
     <>
-      <div className="flex flex-row">
-        <Sidebar closeSidebar={handleMobileSidebar} isOpen={sidebar} handleSelectFilter={handleSelectFilter} />
-        <Recipes key={filters.length} openSidebar={handleMobileSidebar} filters={filters} handleAddFilter={handleSelectFilter} />
+      <div className="flex">
+        {/* <Sidebar closeSidebar={handleMobileSidebar} isOpen={sidebar} handleSelectFilter={handleSelectFilter} /> */}
+        <Recipes />
       </div>
     </>
   )
@@ -28,12 +26,6 @@ function App() {
     // Deactivate scroll when menu is open.
     if (!sidebar) document.body.style.overflow = 'hidden'
     else document.body.style.overflow = 'auto'
-  }
-
-  function handleSelectFilter(title) {
-    //filters.push(title)
-    setFilters(filters++)
-    console.log(filters)
   }
 }
 
--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -2,54 +2,32 @@ import { MagnifyingGlassIcon, FunnelIcon } from '@heroicons/react/24/outline'
 
 import React, { useState, useEffect } from 'react';
 
-export default function Recipes({openSidebar, filters, handleAddFilter, counter}) {
-
-    // let [filters, setFilters] = useState([])
-
-
-    useEffect(() => {
-        console.log("lelele")
-        // check this => https://stackoverflow.com/questions/37009328/re-render-react-component-when-prop-changes
-      }, [counter]);
-      console.log("🚀 ~ Recipes ~ filters", filters)
-
-    // handleAddFilter = (filter) => {
-    //     console.log("leggoo")
-    //     filters.push(filter)
-    //     setFilters(filters)
-    // }
+export default function Recipes() {
     
     return (
         <> 
             <div className="flex flex-1 flex-col">
                 <div className="sticky top-0 z-10 flex py-4 flex-shrink-0 bg-white shadow">
-                    <button type="button" className="sm:hidden border-r border-gray-200 px-4 py-2 text-gray-500"
-                            onClick={() => openSidebar()}>
+                    {/* <button type="button" className="hidden border-r border-gray-200 px-4 py-2 text-gray-500"
+                            onClick={() => console.log("not implemented")}>
                         <span className="sr-only">Open sidebar</span>
                         <div className='relative'>
                             <FunnelIcon className="block h-6 w-6"/>
                             <div className='h-2 w-2 bg-yellow-600 rounded-full absolute top-0 right-0'></div>
                         </div>
-                    </button>
+                    </button> */}
                     <div className="w-full p-2 text-gray-400 focus-within:text-gray-600 flex gap-4">
                         <div className="pointer-events-none flex items-center justify-center">
                             <MagnifyingGlassIcon className="block h-6 w-6"/>
                         </div>
-                        <input id="search-field" className=" h-full w-full border-transparent text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm" placeholder="Search" type="search" name="search"/>
+                        <input id="search-field" className=" h-full w-full border-transparent text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0" placeholder="Search" type="search" name="search"/>
                     </div>
                 </div>
 
                 <main className="flex-1 pt-4 px-4">
                     <div className="max-w-7xl">
                     <div className="flex flex-col gap-2">
-                            <h1 className="text-2xl font-semibold text-gray-900">Filters</h1>
-                            <div key={filters.length} className="h-96 rounded-lg border-4 border-dashed border-gray-200">
-                            <div className='text-red-500'>
-                                                 {counter}
-                                         </div>
-                                
-                                
-                            </div>
+                            <h1 className="text-2xl font-semibold text-gray-900">Recipes</h1>
                         </div>
                     </div>
                 </main>
