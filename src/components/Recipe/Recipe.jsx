3604e66df55dbb682596c5ecb89c2a8ff4b1b0a2
Steven Gonzalez <stevengonzalez957@gmail.com>
2023-02-27 16:30:53 -0600

    set width

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -5,7 +5,7 @@ const Recipe = ({healthLabels, title, calories, image, servings, sourceUrl}) => 
         <div className="flex flex-col min-w-[300px] max-w-[600px] bg-gray-100 shadow-md rounded-md border cursor-pointer"
             onClick={() => window.open(sourceUrl)}>
             <div className="flex flex-col sm:flex-row p-2 gap-2">
-                <img className="h-36 sm:w-36 rounded-md" src={image} alt=""/>
+                <img className="h-36 w-36 rounded-md" src={image} alt=""/>
 
                 <div className="w-full flex flex-col gap-2">
                     <div className="overflow-hidden sm:whitespace-nowrap max-w-[400px] font-extrabold text-gray-900 text-ellipsis">{title}</div>
